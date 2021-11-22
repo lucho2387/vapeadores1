03df8e64b7b3a3174beddb87f5c901a7beb19e5a
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-11-22 18:39:03 -0300

    Update ModalCarrito.jsx

--- src/component/CardWidget/ModalCarrito.jsx
+++ src/component/CardWidget/ModalCarrito.jsx
@@ -1,39 +1,40 @@
 import React, { useState } from "react";
 import ItemCount from '../ItemCount/ItemCount'
 import './modalCarrito.css'
-import img from "../../img/vaporizador1.png"
-import img2 from "../../img/vaporizador2.png"
-import img3 from "../../img/vaporizador3.png"
+import img from "../../assets/vaporizador1.png"
+import img2 from "../../assets/vaporizador2.png"
+import img3 from "../../assets/vaporizador3.png"
 import ModalPago from "../Modal/ModalPago";
 
-const modalCarrito = ({ isOpen, closeCarrito, titulo }) => {
+const modalCarrito = ({ isOpen, closeCarrito, titulo, totalCompra}) => {
     
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [isOpenPago, setIsOpenPago] = useState(false);
 
-    const openPago = () => {
+    const onOpenPago = () => {
         setIsOpenPago(true)
     }
-     const closePago = () => {
+    
+    const onClosePago = () => {
         setIsOpenPago(false)
     }
-
+    
     return (
         <div className={`modalCarrito ${isOpen && 'modalCarrito-open'}`}>
             <h1 className="tituloCarrito">{titulo}</h1>
             <button className="botonCerrarCarrito" onClick={closeCarrito}>
-                    X
-                </button>
-            <ItemCount nombre="VAPORESSO SKY SOLO PLUS" img={img} precio="400"/>
-            <ItemCount nombre="VAPE PEN V2" img={img2} precio="250"/>
-            <ItemCount nombre="Kit Voopoo Argus" img={img3} precio="600"/>
-            <h3>Total de la Compra <span>$1250</span></h3>
+                X
+            </button>
+            <ItemCount nombre="VAPORESSO SKY SOLO PLUS" img={img} precio="400" stock="10"/>
+            <ItemCount nombre="VAPE PEN V2" img={img2} precio="250" stock="5"/>
+            <ItemCount nombre="Kit Voopoo Argus" img={img3} precio="600" stock="4"/>
+            <h3>Total de la Compra <span>${totalCompra}</span></h3>
             <div className="containerPago">
-                <button className="botonPago" onClick={openPago}>Realizar Pago</button>
+                <button className="botonPago" onClick={onOpenPago}>Realizar Pago</button>
             </div>
             <ModalPago
                 isOpen={isOpenPago}
-                closePago={closePago}
+                closePago={onClosePago}
             />
         </div>
     )
